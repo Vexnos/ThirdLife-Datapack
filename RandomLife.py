8b3ae201984472d1061fc111ff87461800a9d9d7
Vexnos <88638499+Vexnos@users.noreply.github.com>
2023-03-23 12:08:01 +1300

    Modified the Life Generator to use text files

--- RandomLife.py
+++ RandomLife.py
@@ -10,28 +10,6 @@ Project: Random Life generation
 from random import randint
 
 #-------Functions-------
-def how_many_players():
-    # Validates user input
-    while True:
-        try:
-            amount_of_players = int(input("How many players do you have? "))
-            return amount_of_players # Return the amount of players
-        except ValueError:
-            print("Invalid value entered! Please make sure your value is a number!")
-
-def adding_players(amount_of_players):
-    for i in range(amount_of_players): # Allows the user to add the amount of players to the game
-        validating = True # Validates user input
-        while validating:
-            player_name = input("Add a player name here: ")
-            if len(player_name) > 0: # Make sure the name is not just empty
-                players.append(player_name)
-                validating = False
-            else:
-                print("Invalid name entered, please try again")
-
-    return players # Return the players
-
 def roll():
     for player in players:
         life_roll = randint(2, 6)
@@ -41,20 +19,14 @@ def roll():
 if __name__ == "__main__":
     # Defines the list of players
     players = []
-    
-    confirming = True
-    while confirming:
-        # Calls the function to get the amount of players in the game
-        amount_of_players = how_many_players()
-        # Calls the function for the user to add the players to the game
-        players = adding_players(amount_of_players)
+    # Opens the players text file
+    players_list = open('players.txt', 'r')
+    # Reads the lines of the text file
+    lines = players_list.readlines()
 
-        # Confirm the roll, if not the user gets to start again
-        confirm = input("Are you ready to roll your lives? (y/n): ")
-        if confirm.lower().startswith("y"):
-            confirming = False
-        else:
-            confirming = True
+    # Iterates over lines to retrieve player names
+    for line in lines:
+        players.append(line.strip()) # Strips the players of the new line character
         
     # Roll the amount of lives for each player
     life_roll = roll()
